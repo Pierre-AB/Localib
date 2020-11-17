c2cffd03a48b03567d254181e33d708edb324b19
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-11-17 21:44:26 +0100

    signup ok

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -14,8 +14,11 @@ const User = require('../models/user-model');
 
 authRoutes.post('/signup', (req, res, next) => {
 
-  const { email, password, type, fullName, address, zip, geoloc, phone, description, openingHours, picture, businessType } = req.body;
+  const { email, password, type, fullName, address, zip, geoloc, phone, description, openingHours, picture, siret, numVAT, businessType } = req.body;
   // const { email, password } = req.body;
+
+
+  // console.log(req.session.currentUser)
 
   if (!email) {
     res.status(400).json({ message: "please provide an email address" })
@@ -28,39 +31,66 @@ authRoutes.post('/signup', (req, res, next) => {
     return;
   }
 
-if (!type) {
-  res.status(400).json({message: "Please check code, type of user is MANDATORY"})
-  return;
-}
+  if (!type) {
+    res.status(400).json({ message: "Please check code, type of user is MANDATORY" })
+    return;
+  }
 
-if (type === 'store') {
-  // ATTENTION: PICTURE IS NOT IMPLEMENTED YET
-  if(!fullName || !address || !zip || !phone || !description || !openingHours || !siret)
+  //STORE CREATION
 
+  if (type === 'store') {
+    // ATTENTION: PICTURE IS NOT IMPLEMENTED YET
+    if (!fullName || !address || !zip || !phone || !description || !openingHours || !siret || !numVAT || !businessType) {
+      res.status(412).json({ message: "Please fill-in all fields marked with *" });
+      return;
+    }
 
-}
+    User.findOne({ siret })
+      .then(store => {
+        if (store) {
+          res.status(400).json({ message: "This store is already existing" });
+          return;
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        const newStore = new User({
+          email,
+          password: hashPass,
+          type,
+          fullName,
+          address,
+          zip,
+          geoloc,
+          phone,
+          description,
+          openingHours,
+          picture,
+          siret,
+          numVAT,
+          businessType
+        });
+
+        newStore.save()
+          .then(newStore => {
+            req.session.currentUser = newStore;
+            res.status(200).json(newStore);
+          })
+          .catch(err => {
+            res.status(500).json({ message: 'Saving newStore to database went wrong.' })
+          })
+
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Siret check went bad" })
+      })
+
+  }
   // res.json({ message: "link ok" })
 
 
-  const salt = bcrypt.genSaltSync(10);
-  const hashPass = bcrypt.hashSync(password, salt);
 
-  const newUser = new User ({
-    email,
-    password: hashPass,
-    type,
-    fullName,
-    address,
-    zip,
-    geoloc,
-    phone,
-    description,
-    openingHours,
-    picture,
-    businessType
-  });
-
-  newUser.save().then().catch()
 
 })
 

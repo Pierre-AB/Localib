45c0569c9a3d6a0b8768fdf275e479ad2cc42acb
Gonzalo Cappa <goncappa@gmail.com>
2020-11-18 19:28:48 +0100

    comments & titles

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -3,23 +3,52 @@ const mongoose = require('mongoose');
 const Product = require('../models/product-model');
 const router = express.Router();
 
+/*                                                                                                           ,,                                   
+`7MM"""Mq.                        `7MM                      mm           
+  MM   `MM.                         MM                      MM           
+  MM   ,M9 `7Mb,od8 ,pW"Wq.    ,M""bMM `7MM  `7MM  ,p6"bo mmMMmm ,pP"Ybd 
+  MMmmdM9    MM' "'6W'   `Wb ,AP    MM   MM    MM 6M'  OO   MM   8I   `" 
+  MM         MM    8M     M8 8MI    MM   MM    MM 8M        MM   `YMMMa. 
+  MM         MM    YA.   ,A9 `Mb    MM   MM    MM YM.    ,  MM   L.   I8 
+.JMML.     .JMML.   `Ybmd9'   `Wbmd"MML. `Mbod"YML.YMbmd'   `MbmoM9mmmP'                                                                                                                                                
+*/
 
 
+/***
+ *    ########  ######## ########    ###    #### ##        ######  
+ *    ##     ## ##          ##      ## ##    ##  ##       ##    ## 
+ *    ##     ## ##          ##     ##   ##   ##  ##       ##       
+ *    ##     ## ######      ##    ##     ##  ##  ##        ######  
+ *    ##     ## ##          ##    #########  ##  ##             ## 
+ *    ##     ## ##          ##    ##     ##  ##  ##       ##    ## 
+ *    ########  ########    ##    ##     ## #### ########  ######  
+ */
+// Voir un produit
 router.get('/products/:id', (req, res, next) => {
   Product.findById(req.params.id)
     .then(product => {
-      res.json(product);
+      res.json(product); // return le produit
     })
     .catch(error => {
       res.json(error);
     });
 });
 
-router.post('/products', (req, res, next) => {
+/***
+ *     ######  ########  ########    ###    ######## ######## 
+ *    ##    ## ##     ## ##         ## ##      ##    ##       
+ *    ##       ##     ## ##        ##   ##     ##    ##       
+ *    ##       ########  ######   ##     ##    ##    ######   
+ *    ##       ##   ##   ##       #########    ##    ##       
+ *    ##    ## ##    ##  ##       ##     ##    ##    ##       
+ *     ######  ##     ## ######## ##     ##    ##    ######## 
+ */
 
-  const { store_id, name, description, picture, price, category,Tags } = req.body;
+ router.post('/products', (req, res, next) => {
+  const storeId = req.session.user.id
+  const { name, description, picture, price, category,Tags } = req.body;
   Product.create({
-    store_id, 
+    store_id = storeId, // à tester
     name, 
     description, 
     picture, 
@@ -35,7 +64,15 @@ router.post('/products', (req, res, next) => {
     });
 });
 
-// PUT route => to update a specific product
+/***
+ *    ##     ## ########  ########     ###    ######## ######## 
+ *    ##     ## ##     ## ##     ##   ## ##      ##    ##       
+ *    ##     ## ##     ## ##     ##  ##   ##     ##    ##       
+ *    ##     ## ########  ##     ## ##     ##    ##    ######   
+ *    ##     ## ##        ##     ## #########    ##    ##       
+ *    ##     ## ##        ##     ## ##     ##    ##    ##       
+ *     #######  ##        ########  ##     ##    ##    ######## 
+ */
 router.put('/products/:id', (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     res.status(400).json({ message: 'Specified id is not valid' });
@@ -51,6 +88,15 @@ router.put('/products/:id', (req, res, next) => {
     });
 });
 
+/*
+ *    ########  ######## ##       ######## ######## ######## 
+ *    ##     ## ##       ##       ##          ##    ##       
+ *    ##     ## ##       ##       ##          ##    ##       
+ *    ##     ## ######   ##       ######      ##    ######   
+ *    ##     ## ##       ##       ##          ##    ##       
+ *    ##     ## ##       ##       ##          ##    ##       
+ *    ########  ######## ######## ########    ##    ######## 
+ */
 router.delete('/products/:id', (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     res.status(400).json({ message: 'Specified id is not valid' });

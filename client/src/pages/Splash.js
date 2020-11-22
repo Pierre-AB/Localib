c0a8d7a8bcf10b3541aad710a0004daaa797ee0e
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-11-22 22:59:11 +0100

    merge

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Splash.css';
 
-import { Link } from 'react-router-dom';
+
 
 class Splash extends Component {
   render() {
     return (
-<<<<<<< HEAD
+
       <div className="splash-page">
         <h1>Welcome to Localib</h1>
         <p>Une super catchphrase qui tue sa mémère pour attirer tout le monde à utiliser notre appli</p>

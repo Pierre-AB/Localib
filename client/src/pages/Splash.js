a66571261e5fad1d1f42554b2ecf2634be910c2c
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-11-22 23:06:02 +0100

    commit

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -12,7 +12,7 @@ class Splash extends Component {
         <h1>Welcome to Localib</h1>
         <p>Une super catchphrase qui tue sa mémère pour attirer tout le monde à utiliser notre appli</p>
         <Link to='#' className="btn green" >LES COMMERCES AUTOUR DE MOI</Link>
-        <Link to={'/signup'} className="btn dark" params={{ type: "store" }} className="below-nav">JE SUIS UN COMMERCANT</Link>
+        <Link to={'/signup'} className="btn dark" params={{ type: "store" }}>JE SUIS UN COMMERCANT</Link>
       </div>
     );
   }

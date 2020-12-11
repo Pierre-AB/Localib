01e71940c905e70a51b7ff9db56fb1becd93f3f0
Gonzalo Cappa <goncappa@gmail.com>
2020-12-11 22:38:49 +0100

    Geocoding merged for deploy

--- configs/cloudinary.config.js
+++ configs/cloudinary.config.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: { 
-    folder: 'lolalib', 
+    folder: 'localib', 
     allowedFormats: ['jpg', 'png'],
     public_id: (req, file) => file.originalname 
   }
